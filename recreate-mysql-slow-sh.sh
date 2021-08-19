1b687976f7a7124a6da4e6c3af4f00084b60dca9
Yu-k-29 <Huyunisakuhananoyouni@gmail.com>
2021-08-20 01:24:53 +0900

    mysql-slow.logの再生成

---
+++ recreate-mysql-slow-sh.sh
@@ -0,0 +1,19 @@
+TARGET_FILE=/var/log/mysql/mysql-slow.log
+del_flag="$false"
+
+if [ -e $TARGET_FILE ]; then
+    rm -rf /var/log/mysql/mysql-slow.log
+    echo "Delete mysql-slow.log!!"
+    echo ""
+    del_flag = "$true"
+fi
+
+if [ "$del_flag"== "$true"]; then
+    echo "mysql restart..."
+    sudo service mysql restart
+    echo "complete!!"
+    echo ""
+    sudo chmod 777 /var/log/mysql/mysql-slow.log
+
+echo "Recreate mysql-slow.log is complete!!"
+fi
