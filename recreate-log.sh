e7a009bfcfff89846e1e9025369915d3982aebeb
Yu-k-29 <Huyunisakuhananoyouni@gmail.com>
2021-08-21 02:59:32 +0900

    再生成用シェルスクリプトを分割、2つを実行するシェルスクリプトの作成

---
+++ recreate-log.sh
@@ -0,0 +1,4 @@
+./recreate-mysql-slow-sh.sh
+./recreate-access-test-sh.sh
+
+echo "all complete"
--- recreate-mysql-slow-sh.sh
+++ recreate-mysql-slow-sh.sh
@@ -1,33 +1,13 @@
 TARGET_FILE=/var/log/mysql/mysql-slow.log
-TARGET_FILE2=/var/log/nginx/access_test.log
-TARGET_FOLDER=/var/log/nginx
-
 
 if [ -e $TARGET_FILE ]; then
     rm -rf $TARGET_FILE
     echo "Delete $TARGET_FILE"
-    echo ""
-fi
-
-if [ -e $TARGET_FILE2 ]; then
-    sudo chmod 777 $TARGET_FOLDER
-    sudo chmod 777 $TARGET_FILE2
-    rm -rf $TARGET_FILE2
-    echo "Delete $TARGET_FILE2"
-    echo ""
 fi
 
 echo "mysql restart..."
 sudo service mysql restart
 echo "complete!!"
-echo ""
 sudo chmod 777 $TARGET_FILE
 
-echo "nginx restart..."
-sudo service nginx restart
-echo "complete!!"
-echo ""
-sudo chmod 755 $TARGET_FOLDER
-sudo chmod 644 $TARGET_FILE2
-
-echo "Recreate Log Files are complete!!"
+echo "Recreate $TARGET_FILE are complete!!"
